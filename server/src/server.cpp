2afb26a875007b3811640b7aba5e4e98140febcb
John Sullivan <jpsullivan2019@gmail.com>
2020-11-02 00:37:25 -0500

    adds client structure for server

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -5,8 +5,23 @@
 #include <iostream>
 
 // custom includes
+#include <"../include/pool.hpp">
 
 
 // MAGIC NUMBERS
 // maximum number of clients allowed
 #define MAX_CLIENT_COUNT 50
+
+// client struct
+// needs to be constant size for pool allocator to work right
+struct client {
+
+    // client identifier
+    char ident[32];
+    // client's current temp
+    int curr_temp;
+    // last 10 temps received from client
+    int last_temps[10];
+    // client's error state
+    bool is_oob;
+};
