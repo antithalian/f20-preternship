5d0c1bf0668e6fc08f4f9fee886b11d9614cfeef
jacoblarget <41126371+jacoblarget@users.noreply.github.com>
2020-11-02 15:40:25 -0500

    Update server.cpp
    main functionality update again

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -30,14 +30,44 @@ struct client {
 int main(void) {
     
     // initialize fixed pool of memory
-    pool((MAX_CLIENT_COUNT * sizeof(client)), sizeof(client));
+    nicu_pool = pool((MAX_CLIENT_COUNT * sizeof(client)), sizeof(client));
+    nicu_pool.initialize();
     
     // initialize track of how many clients 
     int current_client_count = 0;
-    
-    while(MAX_CLIENT_COUNT > current_client_count) {
-    
-    }
+   
+    // event driven development TO BE PROPERLY CODED WEEK 2 IN ORDER TO ENSURE CODE INTEGRATION STANDARDS
+    /*
+        bool is_good = 1;
+        *client clients[50];
+        while(is_good) {
+            if(new_client_connected) {
+                clients[current_client_count] = nicu_pool.allocate(sizeof(client));
+                current_client_count++;
+                
+            }
+            
+            // first main error to check
+            for(int i = 0; i < current_client_count; ++i) {
+                if(&clients[i].is_oob) {
+                    is_good = 0;
+                    exit("Out of Bounds Error on Client %d", i);
+                }
+            }
+            // second main error to check
+            if(MAX_CLIENT_COUNT < client_current_count + 1) {
+                is_good = 0;
+                exit("Out of Bounds Error for the Pool");
+            }
+            
+        }
+        
+        
+        // when the code exits, the pool needs to be freed. Maybe this can be made into a helper function and put prior to the exit statements?
+        for(int i = current_client_count; i >= 0; --i) {
+            nicu_pool.free(clients[i]);
+        }
+    */
     
     
     return 0;
