d47842fe3703cde7a3a466f10b530846e017b72b
jacoblarget <41126371+jacoblarget@users.noreply.github.com>
2020-11-08 17:23:48 -0500

    Update dummy.cpp

--- dummy-device/src/dummy.cpp
+++ dummy-device/src/dummy.cpp
@@ -2,13 +2,27 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+bool get_command() {
+}
+double send_temperature(bool temp_status) {
+  if(temp_status == 0) {
+    srand(1);
+  }
+  
+  next_temp = 99.15 + 5.5*sin(rand()%100);
+  
+  return next_temp;
+}
 int main() {
+  srand(1);
   double next_temp;
   // we want to ensure that we get all of the tests we need.
   // 97.9 - 100.4 degrees Fahrenheit is typical for babies and children.
   // gives an average starting temperature of 99.15.
-  srand(time(NULL));
-  next_temp =rand()%100;
+  bool temp_status;
+  
+  temp_status = get_command();
+  send_temperature(temp_status);
   
   return 0;
 }
