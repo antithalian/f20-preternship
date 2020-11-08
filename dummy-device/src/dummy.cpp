8c317b21144797c9c0cc9e7e60dc14fecf91b558
jacoblarget <41126371+jacoblarget@users.noreply.github.com>
2020-11-08 16:35:51 -0500

    Update dummy.cpp

---
+++ dummy-device/src/dummy.cpp
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <time.h>
+int main() {
+  // we want to ensure that we get all of the tests we need.
+  // 97.9 - 100.4 degrees Fahrenheit is typical for babies and children.
+  // gives an average starting temperature of 99.15.
+  return 0;
+}
