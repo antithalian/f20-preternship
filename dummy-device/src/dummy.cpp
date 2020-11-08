f435459f5e072827c3a19aee0351947fa9df1a01
Null-Rabbet <69477295+Null-Rabbet@users.noreply.github.com>
2020-11-08 17:46:12 -0500

    Update dummy.cpp

--- dummy-device/src/dummy.cpp
+++ dummy-device/src/dummy.cpp
@@ -6,7 +6,8 @@
 bool get_command(next_temp) {  
   <<next_temp;
   >>temp_status;
-  
+  //This will send the temp data to the server
+  //And recieve the status from the server  
   return temp_status;    
 }
 
@@ -16,13 +17,23 @@ double send_temperature(bool temp_status) {
   }
   
   next_temp = 99.15 + 5.5*sin(rand()%100);
-  
+  //Based on the status of the temperature previously
+  //Either the temp will continue to fluctuat as normal
+  //Or it will be reset to 99.15 if it is out of bounds
   return next_temp;
 }
 
+int end(){
+  >>x
+    return x;
+}
+  
 
 int main() {
   srand(1);
+  int x = 0;
+  
+  while (x == 0){
   double next_temp;
   // we want to ensure that we get all of the tests we need.
   // 97.9 - 100.4 degrees Fahrenheit is typical for babies and children.
@@ -31,7 +42,10 @@ int main() {
   
   temp_status = get_command(next_temp);
   send_temperature(temp_status);
-  
+   
+    pause(2);
+  x = end();
+  }
   
   return 0;
 }
