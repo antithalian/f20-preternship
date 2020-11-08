27d998a855b3a8e973a93ba134233904ac08d042
jacoblarget <41126371+jacoblarget@users.noreply.github.com>
2020-11-08 17:11:59 -0500

    Update dummy.cpp

--- dummy-device/src/dummy.cpp
+++ dummy-device/src/dummy.cpp
@@ -1,5 +1,7 @@
 #include <math.h>
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 int main() {
   // we want to ensure that we get all of the tests we need.
   // 97.9 - 100.4 degrees Fahrenheit is typical for babies and children.
