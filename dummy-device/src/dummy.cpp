47c884c33bdc7bae53a74cc13679abeb7c6fb895
jacoblarget <41126371+jacoblarget@users.noreply.github.com>
2020-11-08 17:15:13 -0500

    Update dummy.cpp

--- dummy-device/src/dummy.cpp
+++ dummy-device/src/dummy.cpp
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main() {
+  double next_temp;
   // we want to ensure that we get all of the tests we need.
   // 97.9 - 100.4 degrees Fahrenheit is typical for babies and children.
   // gives an average starting temperature of 99.15.
+  srand(time(NULL));
+  next_temp =rand()%100;
+  
   return 0;
 }
