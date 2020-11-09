33d245a5a3e448a3aec10f5a92232ab8d804f3ee
John Sullivan <jpsullivan2019@gmail.com>
2020-11-09 15:41:54 -0500

    updates server.cpp to use zmq and process better

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -12,6 +12,8 @@
 // MAGIC NUMBERS
 // maximum number of clients allowed
 #define MAX_CLIENT_COUNT 50
+// size of a single message
+#define MSG_SIZE 0xBEEF // TODO - get an actual number
 
 // client struct
 // needs to be constant size for pool allocator to work right
@@ -34,41 +36,55 @@ int main(void) {
     pool nicu_pool = pool((MAX_CLIENT_COUNT * sizeof(client)), sizeof(client));
     nicu_pool.init();
     
-    // initialize track of how many clients 
+    // initialize var to keep track of how many clients are active
     int current_client_count = 0;
+
+    // initialize zmq setup
+    zmq::socket_t zmq_sock = initialize();
    
-    // event driven development TO BE PROPERLY CODED WEEK 2 IN ORDER TO ENSURE CODE INTEGRATION STANDARDS
-    /*
-        bool is_good = 1;
-        *client clients[50];
-        while(is_good) {
-            if(new_client_connected) {
-                clients[current_client_count] = nicu_pool.allocate(sizeof(client));
-                current_client_count++;
+    bool is_good = 1;
+    client* clients[50];
+    
+    while(is_good) {
+
+        // set up for send buffer
+        char * send_buffer[MSG_SIZE];
+        // set up for recv buffer
+        char * recv_buffer[MSG_SIZE];
+        
+        if(new_client_connected) {
+            
+            // receive from socket
+            bool success = zmq_receive(zmq_sock, &recv_buffer);
+
+            // need to handle recv_buffer contents here
+
+            clients[current_client_count] = nicu_pool.alloc(sizeof(client));
+            current_client_count++;
                 
-            }
+        }
             
-            // first main error to check
-            for(int i = 0; i < current_client_count; ++i) {
-                if(&clients[i].is_oob) {
-                    is_good = 0;
-                    exit("Out of Bounds Error on Client %d", i);
-                }
-            }
-            // second main error to check
-            if(MAX_CLIENT_COUNT < client_current_count + 1) {
+        // first main error to check
+        for(int i = 0; i < current_client_count; ++i) {
+            if(&clients[i].is_oob) {
                 is_good = 0;
-                exit("Out of Bounds Error for the Pool");
+                exit("Out of Bounds Error on Client %d", i);
             }
+        }
+        // second main error to check
+        if(MAX_CLIENT_COUNT < client_current_count + 1) {
+            is_good = 0;
+            exit("Out of Bounds Error for the Pool");
+        }
             
-        }
+    }
         
         
-        // when the code exits, the pool needs to be freed. Maybe this can be made into a helper function and put prior to the exit statements?
-        for(int i = current_client_count; i >= 0; --i) {
-            nicu_pool.free(clients[i]);
-        }
-    */
+    // when the code exits, the pool needs to be freed. Maybe this can be made into a helper function and put prior to the exit statements?
+    for(int i = current_client_count; i >= 0; --i) {
+        nicu_pool.free(clients[i]);
+    }
+
     
     
     return 0;
