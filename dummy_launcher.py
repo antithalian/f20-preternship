72127cea21c0ed22cef6a9d159486e9a423e1a43
John Sullivan <jpsullivan2019@gmail.com>
2020-11-09 15:28:27 -0500

    start work on python script to launch dummies

---
+++ dummy_launcher.py
@@ -0,0 +1,13 @@
+# dummy_launcher.py
+# launches a series of dummy devices
+
+# imports
+from sys import argv
+
+# GLOBALS
+NUMBER_OF_DEVICES = 50
+
+def main():
+
+if __name__ == "__main__":
+    main()
