73b675d658b65e2a59b31d896fc2b16490a40452
John Sullivan <jpsullivan2019@gmail.com>
2020-11-09 15:47:30 -0500

    fleshes out dummy_launcher.py

--- dummy_launcher.py
+++ dummy_launcher.py
@@ -3,11 +3,29 @@
 
 # imports
 from sys import argv
+import subprocess
 
 # GLOBALS
+# max number of devices we support in server.cpp
 NUMBER_OF_DEVICES = 50
 
 def main():
 
+    # check that sys.argv is of the correct length
+    if len(sys.argv) == 3:
+        arg_good = True
+    # if not, print/return error
+    else:
+        print("Wrong number of arguments.")
+        return 1
+
+    if arg_good:
+
+        # launch a series of subprocess dummy devices
+        pass
+    
+    return 0
+
+# we only want to run if we're main, so:
 if __name__ == "__main__":
     main()
