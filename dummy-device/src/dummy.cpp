7014e753fdf701f1c9ed3df17710d4c33f8bc3ce
John Sullivan <jpsullivan2019@gmail.com>
2020-11-15 18:24:07 -0500

    implements most of the response to the server

--- dummy-device/src/dummy.cpp
+++ dummy-device/src/dummy.cpp
@@ -3,6 +3,7 @@
 
 // stdlib includes
 #include <cmath>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -29,22 +30,23 @@ int main(int argc, char* argv[]) {
     unsigned int serial;
     double ampl, shif;
     bool oob_gen;
-    COUT << argc << ENDL;
     if (argc == 5) {
         // serial
-        serial = (unsigned int) atoi(argv[0]);
+        serial = (unsigned int) atoi(argv[1]);
         // amplitude
-        ampl = strtod(argv[1], NULL);
+        ampl = strtod(argv[2], NULL);
         // phase shift
-        shif = strtod(argv[2], NULL);
+        shif = strtod(argv[3], NULL);
         // oob gen
-        oob_gen = (strcmp(argv[3], "true")) ? true : false;
+        oob_gen = (strcmp(argv[4], "true")) ? true : false;
     }
     else {
         return 1;
     }
     // time
     double time = 0;
+    // target temp
+    double target = 99.15;
 
     // create identifier from serial
     std::string ident = create_ident(serial);
@@ -70,6 +72,7 @@ int main(int argc, char* argv[]) {
         // get and set current temperature based on parameters
         double curr_temp = get_temp(time, ampl, shif, oob_gen);
         self.temperature = curr_temp;
+        COUT << curr_temp << ENDL;
         // create a zmq message and put the payload into it
         zmqpp::message send_msg;
         send_msg << self.serialize();
@@ -88,12 +91,17 @@ int main(int argc, char* argv[]) {
         recvd.identifier = recv_str.substr(4, 16);
         recvd.temperature = std::stod(recv_str.substr(20));
 
+        // dbg
+        COUT << recvd.type << " " << recvd.identifier << " " << recvd.temperature << ENDL;
+
         // react to whatever the server said to do
         if (recvd.type == "COMM") {
 
             // if temp is not zero, adjust
             if (recvd.temperature != 0) {
-                // TODO - ADJUST TEMP HERE
+                // obv irl we'd have to send a command to an actual heater
+                target = recvd.temperature;
+                oob_gen = false;
             }
         }
         // if not comm, it'll be kill, so die
@@ -118,7 +126,15 @@ int main(int argc, char* argv[]) {
 // creates an identifier for a client given a serial number for the warmer
 std::string create_ident(unsigned int serial) {
 
-    std::string ret = "CLIENT_WARMER_" + std::to_string(serial);
+    std::string ret;
+    if (serial < 10) {
+        ret = "CLIENT_WARMER_0" + std::to_string(serial);
+    }
+    else {
+        ret = "CLIENT_WARMER_" + std::to_string(serial);
+    }
+
+    COUT << ret << ENDL;
     return ret;
 }
 double get_temp(double time, double ampl, double shif, bool go_oob) {
@@ -129,7 +145,7 @@ double get_temp(double time, double ampl, double shif, bool go_oob) {
 
     // if oob gen is set, take a chance at having it actually go oob
     if (go_oob) {
-        srand(time(NULL));
+        srand((unsigned int) std::time(NULL));
         unsigned int res = rand() % 100;
         // have it maybe go oob if rand() was above 50
         if (res > 75) {
