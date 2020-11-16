4aa6aa44a41b133729722d17b71f76145fc692ed
John Sullivan <jpsullivan2019@gmail.com>
2020-11-15 21:04:36 -0500

    start killer.cpp to have another way to kill serve

---
+++ dummy-device/src/killer.cpp
@@ -0,0 +1,10 @@
+// killer.cpp
+// sends a message to the server to have it kill everything
+
+#include <zmqpp/zmqpp.hpp>
+#include <iostream>
+#include <string>
+
+int main(void) {
+    return 0;
+}
