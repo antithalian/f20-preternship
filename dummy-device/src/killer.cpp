f5c5a4a41d7b5266c1d1f23d97522ce2fb8cba85
John Sullivan <jpsullivan2019@gmail.com>
2020-11-15 21:20:18 -0500

    killer.cpp can now send message to kill server

--- dummy-device/src/killer.cpp
+++ dummy-device/src/killer.cpp
@@ -2,9 +2,34 @@
 // sends a message to the server to have it kill everything
 
 #include <zmqpp/zmqpp.hpp>
+#include "../../shared/msg_util.hpp"
 #include <iostream>
 #include <string>
 
+// MAGIC NUMBERS
+// endpoint for zmq comms
+const std::string ENDPOINT = "tcp://127.0.0.1:55555";
+
 int main(void) {
+
+    // set up zmq client
+    // create context
+    zmqpp::context context;
+    // generate socket
+    zmqpp::socket_type type = zmqpp::socket_type::request;
+    zmqpp::socket socket (context, type);
+    // connect to socket
+    socket.connect(ENDPOINT);
+
+    // create killer payload
+    payload killer = payload("KILL", "KILLER_KILLER_00", 0);
+
+    // put killer string into killer message
+    zmqpp::message kill_msg;
+    kill_msg << killer.serialize();
+    
+    // send killer message with dont_wait
+    socket.send(kill_msg, zmqpp::socket::dont_wait);
+
     return 0;
 }
